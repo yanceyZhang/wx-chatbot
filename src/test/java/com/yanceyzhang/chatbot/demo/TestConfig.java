5c93b155e0931c21be5c9be3dfc9f93f7dc853f9
zhangyang <zhangyang@duckbillscm.com>
2020-11-16 14:44:27 +0800

    .

--- src/test/java/com/yanceyzhang/chatbot/demo/TestConfig.java
+++ src/test/java/com/yanceyzhang/chatbot/demo/TestConfig.java
@@ -5,5 +5,5 @@ package com.yanceyzhang.chatbot.demo;
 public class TestConfig {
 	
     //TODO use YOUR chatbot webhook
-    public static final String CHATBOT_WEBHOOK = "";
+    public static final String CHATBOT_WEBHOOK = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=9190ffee-db4c-475b-a8ae-8512054fa0b";
 }
